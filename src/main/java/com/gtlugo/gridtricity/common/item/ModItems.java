c430a8874b28d06ab7479b041c34a367cf8b405b
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-03-10 03:43:06 -0400

    Create ModItems.java

---
+++ src/main/java/com/gtlugo/gridtricity/common/item/ModItems.java
@@ -0,0 +1,8 @@
+package com.gtlugo.gridtricity.common.item;
+
+import com.gtlugo.gridtricity.Gridtricity;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+  public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Gridtricity.MODID);
+}
